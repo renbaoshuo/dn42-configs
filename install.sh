2274e97fd36b9975e391f1e78e5d05fe663ea1d3
Ren Baoshuo <i@baoshuo.ren>
2021-07-31 23:30:16 +0800

    Fix ROA

--- install.sh
+++ install.sh
@@ -6,7 +6,8 @@ mv /etc/bird/bird.conf /etc/bird/bird.conf.bak
 echo '*** Downloading config files...'
 wget -4 -O /tmp/bird.conf https://raw.githubusercontent.com/renbaoshuo/dn42-configs/master/bird.conf && mv -f /tmp/bird.conf /etc/bird/bird.conf
 wget -4 -O /tmp/ibgp.conf https://raw.githubusercontent.com/renbaoshuo/dn42-configs/master/ibgp.conf && mv -f /tmp/ibgp.conf /etc/bird/ibgp.conf
-wget -4 -q -O /tmp/dn42_roa.conf https://dn42.burble.com/roa/dn42_roa_bird2_46.conf && mv -f /tmp/dn42_roa.conf /etc/bird/dn42_roa.conf
+wget -4 -O /tmp/dn42_roa.conf https://dn42.burble.com/roa/dn42_roa_bird2_4.conf && mv -f /tmp/dn42_roa.conf /etc/bird/dn42_roa.conf
+wget -4 -O /tmp/dn42_roa_v6.conf https://dn42.burble.com/roa/dn42_roa_bird2_6.conf && mv -f /tmp/dn42_roa_v6.conf /etc/bird/dn42_roa_v6.conf
 
 echo '*** Setting bird configs...'
 ip address
@@ -25,7 +26,8 @@ echo '*** Write crontab configs to /etc/crontab ...'
 echo '# Baoshuo DN42 Network
 0 * * * * root wget -4 -q -O /tmp/bird.conf https://raw.githubusercontent.com/renbaoshuo/dn42-configs/master/bird.conf && mv -f /tmp/bird.conf /etc/bird/bird.conf && birdc configure
 0 * * * * root wget -4 -q -O /tmp/ibgp.conf https://raw.githubusercontent.com/renbaoshuo/dn42-configs/master/ibgp.conf && mv -f /tmp/ibgp.conf /etc/bird/ibgp.conf && birdc configure
-0 * * * * root wget -4 -q -O /tmp/dn42_roa.conf https://dn42.burble.com/roa/dn42_roa_bird2_46.conf && mv -f /tmp/dn42_roa.conf /etc/bird/dn42_roa.conf && birdc configure
+0 * * * * root wget -4 -q -O /tmp/dn42_roa.conf https://dn42.burble.com/roa/dn42_roa_bird2_4.conf && mv -f /tmp/dn42_roa.conf /etc/bird/dn42_roa.conf && birdc configure
+0 * * * * root wget -4 -q -O /tmp/dn42_roa_v6.conf https://dn42.burble.com/roa/dn42_roa_bird2_6.conf && mv -f /tmp/dn42_roa_v6.conf /etc/bird/dn42_roa_v6.conf && birdc configure
 ' >> /etc/crontab
 systemctl restart cron
 systemctl status cron
