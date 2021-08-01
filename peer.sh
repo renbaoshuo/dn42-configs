d158d5e97ed3a83c964f2e66e3aa234fbbbbeacd
Ren Baoshuo <i@baoshuo.ren>
2021-08-01 11:17:26 +0800

    Update peering script

--- peer.sh
+++ peer.sh
@@ -4,51 +4,76 @@ echo '*** Informations'
 
 ip a
 
-read -p 'Wireguard Private Key: '     WIREGUARD_PRIVATE_KEY
-read -p 'IPv4 Address: '              OWN_IP
-read -p 'IPv6 Address: '              OWN_IPv6
-read -p 'Peer ASN: '                  PEER_ASN
-read -p 'Listen Port: '               LISTEN_PORT
-read -p 'Peer IPv4 Address: '         PEER_IP
-read -p 'Peer IPv6 Address: '         PEER_IPv6
-read -p 'Peer WireGuard EndPoint: '   PEER_WIREGUARD_ENDPOINT
-read -p 'Peer WireGuard Public Key: ' PEER_WIREGUARD_PUBLIC_KEY
+read -p 'Wireguard Private Key: '           WIREGUARD_PRIVATE_KEY
+read -p 'IPv4 Address: '                    OWN_IP
+read -p 'IPv6 Address: '                    OWN_IPv6
+read -p 'Peer ASN: '                        PEER_ASN
+read -p 'Use Link-local IP Address: [y/N] ' USE_LINK_LOCAL
+read -p 'Peer IPv4 Address: '               PEER_IP
+read -p 'Peer IPv6 Address: '               PEER_IPv6
+read -p 'Peer Link-local Address: '         PEER_LINK_LOCAL
+read -p 'Peer WireGuard EndPoint: '         PEER_WIREGUARD_ENDPOINT
+read -p 'Peer WireGuard Public Key: '       PEER_WIREGUARD_PUBLIC_KEY
 
-echo "# Peer dn42-${PEER_ASN}
+OWN_LINK_LOCAL="fe80::247"
+
+WIREGUARD_CONFIG_FILE="/etc/wireguard/dn42-${PEER_ASN:0-4:4}.conf"
+BIRD_CONFIG_FILE="/etc/bird/peers/dn42-${PEER_ASN:0-4:4}.conf"
+
+echo '*** Writing WireGuard configs...'
+echo "# Peer dn42-${PEER_ASN:0-4:4}
 [Interface]
 PrivateKey = ${WIREGUARD_PRIVATE_KEY}
-ListenPort = ${LISTEN_PORT}
-PostUp     = ip addr add fe80::247/64 dev %i
-PostUp     = ip addr add ${OWN_IPv6}/128 peer ${PEER_IPv6}/128 dev %i
-PostUp     = ip addr add ${OWN_IP}/32 peer ${PEER_IP}/32 dev %i
+ListenPort = 4${PEER_ASN:0-4:4}" > $WIREGUARD_CONFIG_FILE
+
+if [[ $USE_LINK_LOCAL =~ ^[Yy](es)?$ ]]; then
+    echo "PostUp     = ip addr add ${OWN_LINK_LOCAL}/128 peer ${PEER_LINK_LOCAL}/128 dev %i" >> $WIREGUARD_CONFIG_FILE
+fi
+
+if [[ $PEER_IPv6 != "" ]]; then
+    echo "PostUp     = ip addr add ${OWN_LINK_LOCAL}/64 dev %i
+PostUp     = ip addr add ${OWN_IPv6}/128 peer ${PEER_IPv6}/128 dev %i" >> $WIREGUARD_CONFIG_FILE
+fi
+
+echo "PostUp     = ip addr add ${OWN_IP}/32 peer ${PEER_IP}/32 dev %i
 Table      = off
 
 [Peer]
 PublicKey  = ${PEER_WIREGUARD_PUBLIC_KEY}
 Endpoint   = ${PEER_WIREGUARD_ENDPOINT}
 AllowedIPs = 0.0.0.0/0, ::/0
-" > /etc/wireguard/dn42-$PEER_ASN.conf
+" >> $WIREGUARD_CONFIG_FILE
 
-systemctl enable --now wg-quick@dn42-$PEER_ASN
-systemctl status wg-quick@dn42-$PEER_ASN
+systemctl enable --now wg-quick@dn42-${PEER_ASN:0-4:4}
+systemctl status wg-quick@dn42-${PEER_ASN:0-4:4}
 
 echo '*** Sleep for 10 seconds.'
 sleep 10
 
-wg show dn42-$PEER_ASN
+echo '*** WireGuard tunnel details:'
+wg show dn42-${PEER_ASN:0-4:4}
 
+echo '*** Writing BIRD configs...'
+echo "# Peer dn42-${PEER_ASN:0-4:4}
+protocol bgp dn42_${PEER_ASN:0-4:4} from dnpeers {" > $BIRD_CONFIG_FILE
 
-echo "# Peer dn42-${PEER_ASN}
-protocol bgp dn42_${PEER_ASN} from dnpeers {
-    neighbor ${PEER_IPv6} as ${PEER_ASN};
-}
-" > /etc/bird/peers/dn42-$PEER_ASN.conf
+if [[ $USE_LINK_LOCAL =~ ^[Yy](es)?$ ]]; then
+    echo "    neighbor ${PEER_LINK_LOCAL} % 'dn42-${PEER_ASN:0-4:4}' as ${PEER_ASN};" >> $BIRD_CONFIG_FILE
+else
+    echo "    neighbor ${PEER_IPv6} as ${PEER_ASN};" >> $BIRD_CONFIG_FILE
+fi
+
+echo "}" >> $BIRD_CONFIG_FILE
+
 
 birdc c
 
-birdc s p dn42_$PEER_ASN
+birdc s p dn42_${PEER_ASN:0-4:4}
 
 echo '*** Sleep for 10 seconds.'
 sleep 10
 
-birdc s p a dn42_$PEER_ASN
+echo '*** BGP session details:'
+birdc s p a dn42_${PEER_ASN:0-4:4}
+
+exit 0
